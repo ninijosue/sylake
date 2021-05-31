382450e3d00a9cac69d9719db333835e7fd88db3
ninijosue <ninijosue123@gmail.com>
2021-06-01 01:18:06 +0200

    upgrading project firebase configurations

--- src/db/index.js
+++ src/db/index.js
@@ -6,29 +6,28 @@ import 'firebase/storage';
 import 'firebase/auth';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-let firebaseConfig = {
-    apiKey: "AIzaSyAK73lUPFpB-4JYaIIWy0dMWc472jGyFyI",
-    authDomain: "rodney-m-s.firebaseapp.com",
-    databaseURL: "https://rodney-m-s.firebaseio.com",
-    projectId: "rodney-m-s",
-    storageBucket: "rodney-m-s.appspot.com",
-    messagingSenderId: "328366471938",
-    appId: "1:328366471938:web:a8a960377a79d289db601b",
-    measurementId: "G-CD18VPM6QG"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyBSlochE6v3ljjzkhxzzI-yJuDaX4s2DEk",
+  authDomain: "sylake-b9510.firebaseapp.com",
+  projectId: "sylake-b9510",
+  storageBucket: "sylake-b9510.appspot.com",
+  messagingSenderId: "230639730649",
+  appId: "1:230639730649:web:277521c6e20e8c3fe6e3c8",
+  measurementId: "G-ZQN45JCE4X"
+};
 
- const PRODUCTION = false;
+const PRODUCTION = false;
 
-  const AppDB = firebase.initializeApp(firebaseConfig);
+const AppDB = firebase.initializeApp(firebaseConfig);
 
-  const AppFirestore = AppDB.firestore();
-  if(!PRODUCTION){
-    AppDB.auth().useEmulator("http://localhost:9099");
-    AppDB.functions().useEmulator("localhost", 5001)
-    AppFirestore.settings({
-      host: "localhost:3000",
-      ssl: false
-    })
-  }
+const AppFirestore = AppDB.firestore();
+if (!PRODUCTION) {
+  AppDB.auth().useEmulator("http://localhost:9099");
+  AppDB.functions().useEmulator("localhost", 5001)
+  AppFirestore.settings({
+    host: "localhost:3000",
+    ssl: false
+  })
+}
 
-export {AppFirestore, AppDB};
+export { AppFirestore, AppDB };
